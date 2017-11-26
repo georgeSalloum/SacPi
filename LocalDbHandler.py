d649fcc48cf8ef24c06d716ce4d3969a569a16ec
George <george.salloum01@lau.edu>
2017-11-26 17:56:28 +0000

    added android channel

---
+++ LocalDbHandler.py
@@ -0,0 +1,27 @@
+import sqlite3
+import time
+import threading
+from threading import Thread
+from DataCollector import DataCollector
+
+class LocalDbHandler(threading.Thread):
+    
+    
+    def collect_data(self):
+        dataCollector = DataCollector()
+        conn = sqlite3.connect('sac.db');
+        cursor = conn.cursor()
+        print("Connected to SAC db successfully")
+    
+        while(True):
+            temp = dataCollector.get_temp();
+            humidity = dataCollector.get_humidity();
+            query = "INSERT INTO sac_data values(date('now'), time('now'), {}, {})".format(temp,humidity);
+            print(query);
+            cursor.execute(query);
+            conn.commit();
+            time.sleep(60)
+        
+    def run(self):
+        self.collect_data()
+        
