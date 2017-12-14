ba5eeb62a8efc55dd8ed2ef0ef0ef5d86f14a0de
George <george.salloum01@lau.edu>
2017-12-14 14:11:21 +0000

    Auto-mode added

--- TemperatureMonitor.py
+++ TemperatureMonitor.py
@@ -18,52 +18,74 @@ class TemperatureMonitor(threading.Thread):
     
     class SACListener(SubscribeCallback):
         sac_temp = 20
+        auto_mode = 1
             
         def message(self, pubnub, message):
             channel = message.channel
             message = message.message
+            print(message)
+            
             #Android
             if(channel == 'Temperature_Command_Channel_Android'):
-                print(message)
                 message = message['nameValuePairs'];
                 req = message['Request']
                 if(req == 'Get_Data_Load'):
-                    TemperatureMonitor().publish_data(self.sac_temp,'Temperature_Data_Load_Channel')
+                    TemperatureMonitor().publish_data(self.sac_temp,self.auto_mode,'Temperature_Data_Load_Channel')
                 if(req == 'Change_Temp'):
                     new_temp = message['Temperature']
                     self.update_temperature(new_temp)
-                    TemperatureMonitor().publish_data(self.sac_temp,'Temperature_Data_Load_Channel')   
+                    TemperatureMonitor().publish_data(self.sac_temp,self.auto_mode,'Temperature_Data_Load_Channel')
+                if(req == 'Auto_Mode_Change'):
+                    new_mode = message['Mode']
+                    self.update_mode(new_mode)
+                    TemperatureMonitor().publish_data(self.sac_temp,self.auto_mode,'Temperature_Data_Load_Channel')
             
             #WebApplication/js
             if(channel == 'Temperature_Command_Channel'):
                 req = message['Request'] 
                 if(req == 'Get_Data_Real_Time'):
-                    TemperatureMonitor().publish_data(self.sac_temp,'Temperature_Real_Time_Channel')
+                    TemperatureMonitor().publish_data(self.sac_temp,self.auto_mode,'Temperature_Real_Time_Channel')
                 if(req == 'Change_Temp'):
                     new_temp = message['Temperature']
                     self.update_temperature(new_temp)
-                    TemperatureMonitor().publish_data(self.sac_temp,'Temperature_Data_Load_Channel')
+                    TemperatureMonitor().publish_data(self.sac_temp,auto_mode,'Temperature_Data_Load_Channel')
+                    
+             #MachineLearner
+            if(channel == 'Temperature_Command_Channel_ML'):
+                req = message['Request']
+                if(req == 'Get_Data_ML'):
+                    TemperatureMonitor().publish_data(self.sac_temp,self.auto_mode,'Temperature_Data_Channel_ML')
+                if(req == 'Change_Temp'):
+                    print('ml updating')
+                    new_temp = message['ml_temperature']
+                    if(self.auto_mode == 1):
+                        self.update_temperature(new_temp)
+                        TemperatureMonitor().publish_data(self.sac_temp,self.auto_mode,'Temperature_Data_Load_Channel')  
     
     
         def presence(self, pubnub, presence):
             pass
         
         def update_temperature(self,new_temp):
-            self.sac_temp = new_temp 
+            self.sac_temp = new_temp
+            
+        def update_mode(self,new_mode):
+            self.auto_mode = new_mode
                 
         
     def publish_callback(self,result,status):
         pass
     
     
-    def publish_data(self,sac_temp,channel_name):
+    def publish_data(self,sac_temp,auto_mode,channel_name):
         dataCollector = DataCollector()
         temp = dataCollector.get_temp()
         humid = dataCollector.get_humidity()
         message = {
             'temperature': temp,
             'humidity': humid,
-            'sac_temperature':sac_temp
+            'sac_temperature':sac_temp,
+            'auto_mode': auto_mode
             }
         if(channel_name == 'Temperature_Real_Time_Channel'):
             message = {'eon':
@@ -72,7 +94,7 @@ class TemperatureMonitor(threading.Thread):
                         'sac_temperature':sac_temp
                         }
                        }
-        print(channel_name)
+        #print(channel_name)
         self.pubnub.publish().channel(channel_name).message(message).async(self.publish_callback)
 
 
@@ -81,6 +103,7 @@ class TemperatureMonitor(threading.Thread):
         self.pubnub.add_listener(listener)
         self.pubnub.subscribe().channels('Temperature_Command_Channel').execute()
         self.pubnub.subscribe().channels('Temperature_Command_Channel_Android').execute()
+        self.pubnub.subscribe().channels('Temperature_Command_Channel_ML').execute()
 
             
     def run(self):
